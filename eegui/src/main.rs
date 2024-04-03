168a4730118fa3e80d7c1cd6453dcd56b122e676
Guohua-Li <licharles35@gmail.com>
2024-04-03 13:15:10 -0400

    Update main.rs

--- eegui/src/main.rs
+++ eegui/src/main.rs
@@ -1,14 +1,35 @@
-mod app;
-mod eplot;
+use std::{
+    thread,
+    time::Duration
+};
 
-use app::MyApp; // use crate::app::MyApp;
+use brainflow::{
+    board_shim::{
+        self,
+        BoardShim, // BoardShim is the main interface for all boards
+    },
+    brainflow_input_params::BrainFlowInputParamsBuilder,
+    BoardIds,
+    BrainFlowPresets,
+};
 
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> Result<(), eframe::Error> {
-    eframe::run_native(
-        "EEGUI",
-        eframe::NativeOptions::default(),
-        Box::new(|cc| Box::new(MyApp::new(cc)))
-    )
+fn main() {
+    board_shim::enable_dev_board_logger().unwrap();
+    let _ = board_shim::set_log_file("brainflow.log"); // log info will go to this file
+    let params = BrainFlowInputParamsBuilder::default().build();
+    let board = BoardShim::new(BoardIds::SyntheticBoard, params).unwrap();
+    board.prepare_session().unwrap(); // print params
+    // buffer_size
+    // streamer_params
+    board.start_stream(45000, "").unwrap(); // streams data and stores it in a ring buffer
+    thread::sleep(Duration::from_secs(5)); // Puts the current thread to sleep for at least 5 sec.
+
+    board.stop_stream().unwrap();
+    // get all data and remove it from the internal buffer
+    let data = board.get_board_data(Some(10), BrainFlowPresets::DefaultPreset).unwrap();
+
+    board.release_session().unwrap(); // release all resources
+    println!("{}", data.len());
+    println!("{:?}", data);
 }
