933e5528a91069fa559d7b2e7be3fc25e37f29fa
Guohua-Li <licharles35@gmail.com>
2024-12-17 10:14:37 -0500

    Update app.rs

--- eegui/demo5/app.rs
+++ eegui/demo5/app.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use std::collections::HashSet;
 use std::collections::HashMap;
 
@@ -288,7 +289,10 @@ pub fn configure_fonts(ctx: &Context) {
    let mut fonts = FontDefinitions::default();
    fonts.font_data.insert(
       "MesloLGS".to_owned(),
-      FontData::from_static(include_bytes!("../fonts/MesloLGS_NF_Regular.ttf"))
+      //FontData::from_static(include_bytes!("../fonts/MesloLGS_NF_Regular.ttf"))
+        Arc::new(FontData::from_static(include_bytes!(
+            "../fonts/MesloLGS_NF_Regular.ttf"
+        ))),
    );
    fonts.families.get_mut(&FontFamily::Proportional).unwrap().insert(0, "MesloLGS".to_owned());
    fonts.families.get_mut(&FontFamily::Monospace).unwrap().push("MesloLGS".to_owned());
